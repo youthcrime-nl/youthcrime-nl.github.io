db98fe306bff1cf3f139cded30c318bc8d6c6848
Arctic Crow (Daniel) <arctic-crow@web.de>
2018-02-16 00:29:48 +0100

    Testing api

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
-var $button;
-$(document).ready(function () {
-    $button = $("button").first();
-    $button.click(function () {
-        $("#text").css("color", "pink");
-        $button.text("Why would you do that to me?!");
-        $button.first().text("Why would you do that to me?!");
+$(function () {
+    var API_URL = "https://opendata.cbs.nl/ODataApi/odata/71930ned/TypedDataSet";
+    //var data;
+    // Look at "http://api.jquery.com/jquery.getjson/#jqxhr-object" for more information about the return type
+    var jqxhr = $.getJSON(API_URL, function (t) {
+        console.log("success");
+        //data = t;
     });
+    // Assign handlers
+    jqxhr.done(function(data) {
+            var items = [];
+            $.each(data.value, function (i, item) {
+                items.push(
+                    "<div id='" + item.id + "'>" +
+                        "<h1>" + item.Perioden + "</h1>" +
+                    "</div>"
+                );
+            });
+
+            var $content = $("<section></section>", {
+                "class": "all-crimes",
+                html: items.join("<hr>")
+            });
+
+            $("body").append($content);
+
+            console.log(data);
+        })
+        .fail(function() {
+            console.log( "error" );
+        })
+        .always(function() {
+            console.log( "complete" );
+        });
 });
 
