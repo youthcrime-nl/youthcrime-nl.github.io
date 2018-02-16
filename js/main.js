8bff6d52ed83586da737c6dde0e23409caab923a
ArcticCrow <arctic-crow@web.de>
2018-02-17 00:50:17 +0100

    Added .gitignore Added all json properties to the listing Added translation for most of the json keys

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(function () {
     var API_URL = "https://opendata.cbs.nl/ODataApi/odata/71930ned/TypedDataSet";
     //var data;
     // Look at "http://api.jquery.com/jquery.getjson/#jqxhr-object" for more information about the return type
-    var jqxhr = $.getJSON(API_URL, function (t) {
+    var jqxhr = $.getJSON(API_URL, function () {
         console.log("success");
         //data = t;
     });
@@ -10,11 +10,16 @@ $(function () {
     jqxhr.done(function(data) {
             var items = [];
             $.each(data.value, function (i, item) {
-                items.push(
-                    "<div id='" + item.id + "'>" +
-                        "<h1>" + item.Perioden + "</h1>" +
-                    "</div>"
-                );
+                var html = "<div id='" + item.ID + "'>" + "<h1>" + (parseInt(item.ID) + 1) + ". Entry" +
+                    "<small><i> ID: " + item.ID + "</i></small></h1>" +
+                    "<ul>";
+
+                $.each(item, function (key, value) {
+                    html += "<li class='" + key + "'>" + translate(key) + ": " + value + "</li>";
+                });
+
+                html += "</ul></div>";
+                items.push(html);
             });
 
             var $content = $("<section></section>", {
@@ -34,3 +39,40 @@ $(function () {
         });
 });
 
+function translate(json_key) {
+
+    switch(json_key) {
+        case "ID": return "ID";
+        case "Geslacht": return "Sex";
+        case "Leeftijd": return "Age";
+        case "Herkomstgroeperingen": return "Origin";
+        case "Perioden": return "Periods";
+        case "TotaalAantalHaltJongeren_1": return "Total nr. of convicted minors";
+        case "TotaalMisdrijvenHalt_2": return "TotaalMisdrijvenHalt_2 (untranslated)";
+        case "GeweldsmisdrijvenHalt_3": return "Violent crimes";
+        case "VernielingEnOpenbareOrdeHalt_4": return "Destruction of public property";
+        case "VermogensmisdrijvenHalt_5": return "Property crimes";
+        case "OverigeMisdrijvenHalt_6": return "Other crimes";
+        case "TotaalOvertredingenHalt_7": return "Total nr. of crimes";
+        case "BaldadigheidHalt_8": return "Wantonness crimes";
+        case "OvertredingLeerplichtwet_9": return "Educational attendance law";
+        case "VuurwerkovertredingenHalt_10": return "Firework violations";
+        case "OverigeOvertredingenHalt_11": return "Other violations";
+        case "OnbekendMisdrijfOfOvertreding_12": return "OnbekendMisdrijfOfOvertreding_12 (untranslated)";
+        case "TotaalAantalHaltJongeren_13": return "TotaalAantalHaltJongeren_13 (untranslated)";
+        case "TotaalMisdrijvenHalt_14": return "TotaalMisdrijvenHalt_14 (untranslated)";
+        case "GeweldsmisdrijvenHalt_15": return "GeweldsmisdrijvenHalt_15 (untranslated)";
+        case "VernielingEnOpenbareOrdeHalt_16": return "VernielingEnOpenbareOrdeHalt_16 (untranslated)";
+        case "VermogensmisdrijvenHalt_17": return "VermogensmisdrijvenHalt_17 (untranslated)";
+        case "OverigeMisdrijvenHalt_18": return "OverigeMisdrijvenHalt_18 (untranslated)";
+        case "TotaalOvertredingenHalt_19": return "TotaalOvertredingenHalt_19 (untranslated)";
+        case "BaldadigheidHalt_20": return "BaldadigheidHalt_20 (untranslated)";
+        case "OvertredingLeerplichtwet_21": return "OvertredingLeerplichtwet_21 (untranslated)";
+        case "VuurwerkovertredingenHalt_22": return "VuurwerkovertredingenHalt_22 (untranslated)";
+        case "OverigeOvertredingenHalt_23": return "OverigeOvertredingenHalt_23 (untranslated)";
+        case "OnbekendMisdrijfOfOvertreding_24": return "OnbekendMisdrijfOfOvertreding_24 (untranslated)";
+
+    }
+
+    return null;
+}
